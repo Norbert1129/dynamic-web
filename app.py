b5813665daaa83a36376e04516b0bea5927546d0
Ubuntu <ubuntu@ip-172-31-3-174.ap-southeast-2.compute.internal>
2026-05-04 02:25:14 +0000

    Add journey from and list page

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+journeys = []
+
+@app.route("/app/journeys")
+def list_journeys():
+    return render_template("journeys.html", journeys=journeys)
+
+@app.route("/app/add", methods=["GET", "POST"])
+def add_journey():
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        journey = {
+            "id": len(journeys) + 1,
+            "title": title,
+            "content": content
+        }
+        journeys.append(journey)
+        return redirect("/app/journeys")
+    return render_template("add.html")
 @app.route("/app/")
 def home():
     return render_template("index.html")

ea2bee2d7cd89ff26ea669d2bab5fe9cff4bbfa3
Norbert <nemethnorbert1129@gmail.com>
2026-05-07 03:58:55 +0000

    migrated static site into flask dynamic app

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-
+\
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 journeys = []
 
-@app.route("/app/journeys")
+@app.route("/journeys")
 def list_journeys():
     return render_template("journeys.html", journeys=journeys)
 
-@app.route("/app/add", methods=["GET", "POST"])
+@app.route("/add", methods=["GET", "POST"])
 def add_journey():
     if request.method == "POST":
         title = request.form.get("title")
@@ -21,17 +21,46 @@ def add_journey():
             "content": content
         }
         journeys.append(journey)
-        return redirect("/app/journeys")
+        return redirect("/journeys")
     return render_template("add.html")
-@app.route("/app/")
+
+@app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/first")
+def first():
+     return render_template("journey1.html")
+
+@app.route("/second")
+def second():
+     return render_template("journey2.html")
+
+@app.route("/third")
+def third():
+    return render_template("journey3.html")
+
+@app.route("/fourth")
+def fourth():
+    return render_template("journey4.html")
+
+@app.route("/fifth")
+def fifth():
+    return render_template("journey5.html")
+
+@app.route("/sixth")
+def sixth():
+    return render_template("journey6.html")
+
+@app.route("/seventh")
+def seventh():
+    return render_template("journey7.html")
 
 @app.route("/about")
 def about():
     return "About page"
 
-@app.route("/app/test")
+@app.route("/test")
 def test():
     return render_template("index2.html")
 
